39be240e76870779681c9ccfb1b2d8964b7008e4
Maureen Ndereba <117657621+maureen-ndereba@users.noreply.github.com>
2023-07-08 00:23:55 +0300

    Completed Assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
-# Your code here!
+def greet_programmer
+   puts "Hello, programmer!"
+end
+
+def greet (name = "Naureen")
+    puts "Hello, #{name}! }"
+end
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}! }"
+end
+greet_with_default 
+greet_with_default "Naureen"
+
+def add(num1,num2)
+    return num1 + num2
+end
+
+def halve(num4)
+    if num4.is_a?(Integer)
+      num4 / 2
+    else
+      nil
+    end
+  end
